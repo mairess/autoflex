77b1fb0d0ea1124a31094eea5bcaf6c99da3ced7
mairess <trabalho.maires@gmail.com>
2026-02-09 21:50:24 -0300

    initial commit

---
+++ backend/src/main/java/com/autoflex/backend/AutoflexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.autoflex.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoflexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoflexBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/autoflex/backend/AutoflexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoflex.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoflexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
